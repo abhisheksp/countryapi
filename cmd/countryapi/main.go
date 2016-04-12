0aa970cc8043c06e114e0bb0cf4ad6ad740c541b
Abhishek SP <abhisp@thoughtworks.com>
2016-04-12 23:53:01 +0530

    <Abhishek> [Capital] Modified to follow heroku deployment standards

--- cmd/countryapi/main.go
+++ cmd/countryapi/main.go
@@ -5,12 +5,19 @@ import (
 	"github.com/gorilla/mux"
 	"log"
 	"net/http"
+	"os"
 )
 
 const GET = "GET"
 
 func main() {
+	port := os.Getenv("PORT")
+
+	if port == "" {
+		log.Fatal("$PORT must be set")
+	}
+
 	router := mux.NewRouter().StrictSlash(true)
 	router.HandleFunc("/capital/{country}", handlers.Capital).Methods(GET)
-	log.Fatal(http.ListenAndServe(":8080", router))
+	log.Fatal(http.ListenAndServe(":"+port, router))
 }
