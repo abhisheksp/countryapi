b5b8b73ce18f2866157d65edc397a9177bdb31d4
Abhishek SP <abhisp@thoughtworks.com>
2016-04-14 23:31:56 +0530

    <Abhishek> [Capital] Added Error state to response

--- handlers/capitalhandler.go
+++ handlers/capitalhandler.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"github.com/abhisheksp/countryapi/dependencyfactory"
 	"github.com/gorilla/mux"
+	"log"
 	"net/http"
 	"strings"
 )
@@ -16,25 +17,29 @@ type CountryInfo struct {
 type CountryResponse struct {
 	Country string `json:"country"`
 	Capital string `json:"capital"`
+	Error   bool   `json:"error"`
 }
 
 func Capital(w http.ResponseWriter, r *http.Request) {
 	vars := mux.Vars(r)
 	country := strings.ToLower(vars["country"])
+	countryResponse := CountryResponse{}
 	capital, found := dependencyfactory.CountryCapitals[country]
 	if !found {
 		w.WriteHeader(http.StatusInternalServerError)
-		fmt.Fprintf(w, "Invalid Country Name")
-		return
-	}
-	countryResponse := CountryResponse{
-		Country: country,
-		Capital: capital,
+		log.Println("[INFO]", country, "Invalid Country Name")
+		countryResponse = CountryResponse{Error: true}
+	} else {
+		countryResponse = CountryResponse{
+			Country: country,
+			Capital: capital,
+		}
 	}
 	response, err := json.Marshal(countryResponse)
 	if err != nil {
 		w.WriteHeader(http.StatusInternalServerError)
-		fmt.Fprintf(w, err.Error())
+		log.Println("[FATAL]", err.Error())
+		countryResponse = CountryResponse{Error: true}
 	}
 	fmt.Fprintf(w, string(response))
 }
